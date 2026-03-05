36babe770e83d2104f752c0c10d61eb945321f65
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2026-03-05 16:51:29 +0100

    all: post-release version bump

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #pragma once
-#define UPX_VERSION_HEX      0x050101 /* 05.01.01 */
-#define UPX_VERSION_STRING   "5.1.1"
-#define UPX_VERSION_STRING4  "5.11"
-#define UPX_VERSION_DATE     "Mar 5th 2026"
-#define UPX_VERSION_DATE_ISO "2026-03-05"
+#define UPX_VERSION_HEX      0x050102 /* 05.01.02 */
+#define UPX_VERSION_STRING   "5.1.2"
+#define UPX_VERSION_STRING4  "5.12"
+#define UPX_VERSION_DATE     "Mar 6th 2026"
+#define UPX_VERSION_DATE_ISO "2026-03-06"
 #define UPX_VERSION_YEAR     "2026"
